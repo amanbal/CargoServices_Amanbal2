de76a970640f27ffdb09eb266b6208e983aacd41
arshbal269@gmail.com <arshbal269@gmail.com>
2021-05-17 23:20:10 +0300

    Some changes

---
+++ CargoServices_Amanbal/Controllers/BookingsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CargoServices_Amanbal.Data;
+using CargoServices_Amanbal.Models;
+
+namespace CargoServices_Amanbal.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bookings
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Bookings.Include(b => b.Customer);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Bookings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings
+                .Include(b => b.Customer)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // GET: Bookings/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "ID");
+            return View();
+        }
+
+        // POST: Bookings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,CustomerID,FromAddress,ToAddress,Date")] Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(booking);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "ID", booking.CustomerID);
+            return View(booking);
+        }
+
+        // GET: Bookings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "ID", booking.CustomerID);
+            return View(booking);
+        }
+
+        // POST: Bookings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,CustomerID,FromAddress,ToAddress,Date")] Booking booking)
+        {
+            if (id != booking.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(booking);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookingExists(booking.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "ID", booking.CustomerID);
+            return View(booking);
+        }
+
+        // GET: Bookings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings
+                .Include(b => b.Customer)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // POST: Bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var booking = await _context.Bookings.FindAsync(id);
+            _context.Bookings.Remove(booking);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookingExists(int id)
+        {
+            return _context.Bookings.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ CargoServices_Amanbal/Controllers/JobsController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CargoServices_Amanbal.Data;
+using CargoServices_Amanbal.Models;
+
+namespace CargoServices_Amanbal.Controllers
+{
+    public class JobsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JobsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Jobs
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Jobs.Include(j => j.Customer).Include(j => j.Driver).Include(j => j.Vehicle);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Jobs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job = await _context.Jobs
+                .Include(j => j.Customer)
+                .Include(j => j.Driver)
+                .Include(j => j.Vehicle)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return View(job);
+        }
+
+        // GET: Jobs/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "ID");
+            ViewData["DriverID"] = new SelectList(_context.Drivers, "ID", "ID");
+            ViewData["VehicleID"] = new SelectList(_context.Vehicles, "ID", "ID");
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,CustomerID,DriverID,VehicleID,StartDate,EndDate,Status")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(job);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "ID", job.CustomerID);
+            ViewData["DriverID"] = new SelectList(_context.Drivers, "ID", "ID", job.DriverID);
+            ViewData["VehicleID"] = new SelectList(_context.Vehicles, "ID", "ID", job.VehicleID);
+            return View(job);
+        }
+
+        // GET: Jobs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job = await _context.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "ID", job.CustomerID);
+            ViewData["DriverID"] = new SelectList(_context.Drivers, "ID", "ID", job.DriverID);
+            ViewData["VehicleID"] = new SelectList(_context.Vehicles, "ID", "ID", job.VehicleID);
+            return View(job);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,CustomerID,DriverID,VehicleID,StartDate,EndDate,Status")] Job job)
+        {
+            if (id != job.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(job);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JobExists(job.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "ID", job.CustomerID);
+            ViewData["DriverID"] = new SelectList(_context.Drivers, "ID", "ID", job.DriverID);
+            ViewData["VehicleID"] = new SelectList(_context.Vehicles, "ID", "ID", job.VehicleID);
+            return View(job);
+        }
+
+        // GET: Jobs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job = await _context.Jobs
+                .Include(j => j.Customer)
+                .Include(j => j.Driver)
+                .Include(j => j.Vehicle)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return View(job);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+            _context.Jobs.Remove(job);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JobExists(int id)
+        {
+            return _context.Jobs.Any(e => e.ID == id);
+        }
+    }
+}
